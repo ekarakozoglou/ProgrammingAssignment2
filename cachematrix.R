a40f441d6182648ca2ee0166ffcc08a3aca9a44e
Evi <ekarakozoglou@datalicious.com>
2014-07-27 14:27:11 +1000

    Assignment 2 submission
    changes to cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,68 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignement 2 - rprog-005
+## Two functions, calculating the inverse of a matrix in two cases: 
+## 1 - initial matrix has not been changed
+## 2 - initial matrix has been changed 
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(), ...){
+  ## create a special "matrix" object that can cache its inverse
+  
+  ## allocate matrix
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  ## set get
+  get <- function() x
+  
+  ## set matrices
+  setmatrix <- function(solve) m <<- solve
+  
+  getmatrix <-function() m
+  
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
+  
 }
 
 
+
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+  ## compute the inverse of the special "matrix" returned by makeCacheMatrix
+  ## If the inverse has already been calculated (and the matrix has not changed)
+  ## then the cachesolve retrieves the inverse from the cache.
+  
+  ## check if inverse is empty
+  m <- x$getmatrix()
+  if(!is.null(m)){
+    ## is the matrix the same?  
+    im <- diag(nrow(m))
+    calcIm <- m %*% x$get()           
+    if(isTRUE(all.equal(calcIm,im))){
+      message("Calculating from cache")
+      return(m)
+    }
+    else{
+      message("Calculating again as matrix change ")
+      data <- x$get()
+      m = solve(data, ...)
+      x$setmatrix(m)
+      return(m)
+    }
+  }
+  
+  ## get matrix
+  data <- x$get()
+  ## calculate inverse
+  m = solve(data, ...)
+  ## set inverse in object x
+  x$setmatrix(m)
+  ## print inverse
+  m
+  #}
+  
+  
 }
